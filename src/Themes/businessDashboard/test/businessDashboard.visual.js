300c0dc30efe3a8769371f9fb4d48b2569980d94
Shlomi Toussia-Cohen <shlomitc@wix.com>
2020-10-15 00:18:58 +0300

    expose reusable viusal tests

--- src/Themes/businessDashboard/test/businessDashboard.visual.js
+++ src/Themes/businessDashboard/test/businessDashboard.visual.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import businessDashboardTheme from '../BusinessDashboard';
-import { buttonTests as ButtonTests } from '../../../Button/test/Button.visual';
-import { circularProgressBarTests as CircularProgressBarTests } from '../../../CircularProgressBar/test/CircularProgressBar.visual';
+import { runTests as ButtonTests } from '../../../Button/test/Button.visual';
+import { runTests as CircularProgressBarTests } from '../../../CircularProgressBar/test/CircularProgressBar.visual';
 import { ThemeProvider } from '../../..';
 
-const themeName = 'BusinessDashboard';
+const themeName = 'Business Dashboard';
 const testWithTheme = test => {
   return (
     <ThemeProvider theme={businessDashboardTheme()}>
